a1248cc82e5aed0971879fbbb1241ed5b78b6f93
Julien Nyambal <Julien.Nyambal@gmail.com>
2024-08-30 10:26:22 +0200

    initial push, peding for info

---
+++ Automated Pull Scripts/CS/CS_repo_clone_MacOS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Cloning the repository with sparse checkout
+git clone --filter=blob:none --sparse https://github.com/skills-cogrammar/C8-Lecture-Backpack.git
+
+# Changing directory to the cloned repository
+cd C8-Lecture-Backpack
+
+# Adding the specific folders to sparse checkout
+git sparse-checkout add "CyberSecurity (CS)"
+git sparse-checkout add "StarterPack"
---
+++ Automated Pull Scripts/CS/CS_repo_clone_Windows.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Cloning the repository with sparse checkout
+git clone --filter=blob:none --sparse https://github.com/skills-cogrammar/C8-Lecture-Backpack.git
+
+REM Changing directory to the cloned repository
+cd C8-Lecture-Backpack
+
+REM Adding the specific folders to sparse checkout
+git sparse-checkout add "CyberSecurity (CS)"
+git sparse-checkout add "StarterPack"
+
+@echo on
