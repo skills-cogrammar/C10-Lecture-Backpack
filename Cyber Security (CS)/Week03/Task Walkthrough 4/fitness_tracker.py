d147cc4b9951d27eaa1e14c2fec3befd75e38d39
Liano-HyperionDev <lianon@hyperiondev.com>
2024-10-22 10:17:52 +0200

    Added slides and live-code

---
+++ Cyber Security (CS)/Week03/Task Walkthrough 4/fitness_tracker.py
@@ -0,0 +1,24 @@
+# import fit_functions
+from fit_functions import workout_summary, running_calories, motivate_me
+
+# monday = workout_summary(20, 15)
+# print(f'\n {'-' * 50}')
+# tuesday = workout_summary(12, 10)
+
+print(motivate_me())
+
+
+# input -> Do something -> output
+# parameter/argument -> adding two arguments -> returning the result
+
+# num_1 = 70
+# num_2 = 35
+# #           Parameters
+# def addition(num_1, num_2):
+#     total = num_1 + num_2
+#     return total
+
+
+#               Arguments
+# total = addition(num_1, num_2)
+# print(total)
