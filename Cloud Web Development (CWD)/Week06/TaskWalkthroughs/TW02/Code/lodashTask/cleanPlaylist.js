5a24659fe533fb09edda195aafc9cca3050e857b
zahra_moham <148523671+zahram-hypdev@users.noreply.github.com>
2024-11-14 16:12:26 +0200

    Updated code for CWD TW02 Week 6

--- Cloud Web Development (CWD)/Week06/TaskWalkthroughs/TW02/Code/lodashTask/cleanPlaylist.js
+++ Cloud Web Development (CWD)/Week06/TaskWalkthroughs/TW02/Code/lodashTask/cleanPlaylist.js
@@ -1,15 +1,26 @@
+const lodash = require('lodash');
+
 // Array with my songs
 let playlist = [
     "Sticky - Tyler the Creator", 
     "Everything is romantic - Charli XCX",
     "Adam - Amine",
     "Sticky - Tyler the Creator",
+    "Baby - Those Guys from Athens",
+    "Everything is romantic - Charli XCX",
+    "Adam - Amine"
 ];
 
 // Display the original playlist
+console.log(`This is your original playlist: 
+    ${playlist}`);
 
 
 // Use lodash to filter out song repeats 
-// import {uniq} from 'lodash';
+let uniquePlaylist = lodash.uniq(playlist);
 
 // Display the new playlist and the number of songs in it
+console.log(`This is your duplicate-free playlist: 
+    ${uniquePlaylist}`);
+console.log(`Your new playlist has ${uniquePlaylist.length} songs in it.`);
+console.log(`We have removed ${playlist.length - uniquePlaylist.length} duplicates.`);
--- Cloud Web Development (CWD)/Week06/TaskWalkthroughs/TW02/Code/momentsTask/birthdayCountdown.js
+++ Cloud Web Development (CWD)/Week06/TaskWalkthroughs/TW02/Code/momentsTask/birthdayCountdown.js
@@ -1,18 +1,23 @@
+const moment = require('moment');
+
 // Create an array of Event objects 
 // Each event must have a name and a date in YYYY-MM-DD format
 let events = [
-    {name: "Zahra's Birthday", date: "2025-11-13"}
+    {name: "Zahra's Birthday", date: "2025-11-13"},
+    {name: "Our last lecture together", date: "2024-12-24"},
+    {name: "Anon's Birthday", date: "2025-09-21"}
 ];
 
 // Calculate the countdown for each event using a function
 function calculateCountdown (events) {
-    events.array.forEach(event => {
+    events.forEach(event => {
         const now = moment();
         const eventDate = moment(event.date);
         const daysUntilEvent = eventDate.diff(now, 'days');
 
-        console.log(`${daysUntilEvent} left until ${event.name}!`);
+        console.log(`${daysUntilEvent} days left until ${event.name}!`);
     });
 }
 
 // Call the function
+calculateCountdown(events);
