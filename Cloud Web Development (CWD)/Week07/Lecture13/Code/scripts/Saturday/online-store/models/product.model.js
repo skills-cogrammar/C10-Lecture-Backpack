7a8954c50e0d87d7ae64b5b549f10b2649d3fbdf
bonganig-cogrammar <152314057+bonganig-cogrammar@users.noreply.github.com>
2024-11-23 13:59:01 +0200

    Add files via upload

---
+++ Cloud Web Development (CWD)/Week07/Lecture13/Code/scripts/Saturday/online-store/models/product.model.js
@@ -0,0 +1,11 @@
+function Product(id, title, price, description, category, image, rating){
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.category = category;
+    this.image = image;
+    this.rating = rating;
+}
+
+export default Product;
