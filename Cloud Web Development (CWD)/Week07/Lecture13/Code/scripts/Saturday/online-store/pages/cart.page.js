80ecd0f3978c89c884198f39adb6493dd2e33228
bonganig-cogrammar <152314057+bonganig-cogrammar@users.noreply.github.com>
2024-11-23 13:59:54 +0200

    Delete Cloud Web Development (CWD)/Week07/Lecture13/Code/scripts/Saturday/online-store directory

--- Cloud Web Development (CWD)/Week07/Lecture13/Code/scripts/Saturday/online-store/pages/cart.page.js
+++
@@ -1,6 +0,0 @@
-export function CartPage(){
-    const container = document.createElement("div");
-    container.innerHTML = "<h2> CART PAGE <h2/>"
-
-    return container;
-}
--- Cloud Web Development (CWD)/Week07/Lecture13/Code/scripts/Saturday/online-store/pages/home.page.js
+++
@@ -1,6 +0,0 @@
-export function HomePage(){
-    const container = document.createElement("div");
-    container.innerHTML = "<h2> HOME PAGE <h2/>"
-
-    return container;
-}
